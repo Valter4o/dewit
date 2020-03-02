80466e66e6992bee13063d4f19cdfa928e84f4da
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-02 21:09:37 +0200

    Added dialog for adding todos

---
+++ src/components/AddTodoDialog.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-text-field
+      v-model="newTodoValue"
+      class="todoInput"
+      :placeholder="placeholder"
+      :clearable="clearable"
+      @keyup.13="submit"
+    />
+
+    <button @click="submit()">
+      Submit
+    </button>
+  </v-dialog>
+</template>
+
+<script>
+import { bus } from '../main';
+
+export default {
+  name: 'AddTodoDialog',
+  data() {
+    return {
+      dialog: true,
+      placeholder: 'Add todo',
+      newTodoValue: '',
+      clearable: true,
+    }
+  },
+  methods: {
+    submit() {
+      bus.$emit('submit', { value: this.newTodoValue });
+      this.newTodoValue = '';
+    }
+  },
+}
+</script>
+
+<style scoped>
+.todoInput {
+  overflow: hidden;
+}
+</style>
