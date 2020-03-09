d1efd1ef6f64ba33ef35223fcd0595e9b45df53e
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-09 20:11:36 +0200

    Removed unnesesary code and left just one generic method

--- src/store/modules/projects.js
+++ src/store/modules/projects.js
@@ -8,38 +8,9 @@ export default {
     },
 
     actions: {
-        fetchProjects({ state, commit }) {
-            return new Promise((resolve, reject) => {
-                firebase
-                    .firestore()
-                    .collection('projects')
-                    .get()
-                    .then((queryS) => {
-                        queryS.forEach((doc) => {
-                            const { name, users } = doc.data()
-                            const user = users.find((u) => u.name === 'Pesho');
-
-
-                            const obj = ({
-                                name,
-                                role: user.role,
-                                participants: users.length,
-                                status: user.status,
-                            })
-
-                            commit('setProject', { obj, id: doc.id })
-                        })
-                        resolve(state.items);
-                    })
-
+        fetchProjects: ({ dispatch }, { ids }) =>
+            dispatch('fetchItems', { ids, resource: 'projects' }, {
+                root: true
             })
-        }
-    },
-
-    mutations: {
-        setProject(state, { obj, id }) {
-            obj['_key'] = id;
-            Vue.set(state.items, id, obj);
-        }
     }
 }
