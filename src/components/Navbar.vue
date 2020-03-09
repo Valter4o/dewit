922f041256fe324858688af3fb9b460c5b87c918
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-09 20:10:00 +0200

    Removed todo list btn

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,8 @@
 <template>
-  <v-app-bar
-    app
-    clipped-left
-    color="red"
-    dense
-  >
+  <v-app-bar app clipped-left color="red" dense>
     <v-app-bar-nav-icon @click.stop="showHideSidebar()" />
     <v-toolbar-title class="mr-12 align-center">
-      <router-link
-        to="/home"
-        class="title"
-      >
+      <router-link to="/home" class="title">
         <span>
           DEWIT
         </span>
@@ -20,36 +12,16 @@
     <v-spacer />
 
     <div class="text-center">
-      <v-btn
-        rounded
-        color="primary"
-        dark
-      >
+      <v-btn rounded color="primary" dark>
         Login
       </v-btn>
     </div>
 
     <div class="text-center">
-      <v-btn
-        rounded
-        color="primary"
-        dark
-      >
+      <v-btn rounded color="primary" dark>
         Register
       </v-btn>
     </div>
-
-    <router-link to="/todolist">
-      <div class="text-center">
-        <v-btn
-          rounded
-          color="primary"
-          dark
-        >
-          Todo List
-        </v-btn>
-      </div>
-    </router-link>
   </v-app-bar>
 </template>
 
@@ -57,10 +29,8 @@
 export default {
   methods: {
     showHideSidebar() {
-      // eslint-disable-next-line no-console
-      console.log('foo');
-      this.$emit('changeSideBar');
-    }
-  }
+      this.$emit('changeSideBar')
+    },
+  },
 }
 </script>
