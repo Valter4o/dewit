3159485c453f983157ca94e08a1729a7e57f4061
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-09 20:11:56 +0200

    Added generic mutation

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,4 +1,9 @@
 import firebase from 'firebase/app'
+import Vue from 'vue'
 
 export default {
+    setItem(state, { item, id, resource }) {
+        item['_key'] = id;
+        Vue.set(state[resource].items, id, item);
+    }
 }
