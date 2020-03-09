d84ffae143d041fa46323992d2e61f4a47163178
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-09 20:10:50 +0200

    Added generic actions to fetch Items from firebase

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,9 +1,20 @@
 import firebase from 'firebase'
 
 export default {
-    foo({ commit }, x) {
-        firebase
-            .firestore()
-            .collection('projects')
+    fetchItem({ commit, state }, { id, resource }) {
+        return new Promise((resolve, reject) => {
+            firebase
+                .firestore()
+                .collection(resource)
+                .doc(id)
+                .onSnapshot((snap) => {
+                    commit('setItem', { item: snap.data(), id, resource })
+                    resolve(state[resource].items[id]);
+                })
+        })
+    },
+
+    fetchItems({ dispatch }, { ids, resource }) {
+        return Promise.all(ids.map(id => dispatch('fetchItem', { id, resource })))
     }
 }
