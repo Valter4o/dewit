21fa8fa40cb6005c1e73209c4d25f45c354008f4
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-10 18:11:14 +0200

    Created routes for projects todo list user list and tasker

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '../views/Home';
-import TodoList from '../views/TodoList';
+import Home from '@/views/Home';
 import ProjectHome from '@/views/ProjectHome'
+import TodoGroups from '@/views/TodoGroups'
+import UsersList from '@/views/UsersList'
+import TaskerMain from '@/views/TaskerMain'
 
 Vue.use(Router);
+// component: () => import(/*webpackChunkName:projectHome*/'./views/ProjectHome'),
 
 export default new Router({
+    mode: 'history',
     routes: [
         {
             path: '/',
@@ -17,12 +21,28 @@ export default new Router({
             component: Home
         },
         {
-            path: '/projects/',
+            path: '/projects/:id',
+            name: 'ProjectHome',
             component: ProjectHome,
-            props: {
-
-            }
+            props: true
+        },
+        {
+            path: '/todoList',
+            name: 'TodoGroups',
+            component: TodoGroups,
+            props: true
+        },
+        {
+            path: '/users',
+            name: 'UsersList',
+            component: UsersList,
+            props: true
+        },
+        {
+            path: '/tasker',
+            name: 'Tasker',
+            component: TaskerMain,
+            props: true
         }
-    ],
-    mode: 'history'
+    ]
 })
