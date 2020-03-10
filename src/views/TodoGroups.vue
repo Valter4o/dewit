701f5c2976df94fa6391e4be194943ed88f42eb6
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-10 18:13:16 +0200

    Created tasker main todoGroups and user list views

---
+++ src/views/TodoGroups.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1>Your Todo Groups</h1>
+    <br />
+    <v-card class="mx-auto" max-width="700">
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-center">Name</th>
+              <th class="text-center">Todos</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="group in groups"
+              :key="group['_key']"
+              @click="showTodoList(group['_key'])"
+            >
+              <td>{{ group.name }}</td>
+              <td>{{ group.todos.length }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  data() {
+    return {
+      groups: [
+        {
+          _key: 0,
+          name: 'Group 1',
+          todos: ['Todo 1', 'Todo 2'],
+        },
+        {
+          _key: 1,
+          name: 'Group 2',
+          todos: ['Todo 1'],
+        },
+      ],
+    }
+  },
+
+  props: {
+    ids: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  methods: {
+    showTodoList(id) {
+      console.log(id)
+    },
+    ...mapActions[('todoGroups', ['fetchGroups'])],
+  },
+
+  created() {},
+}
+</script>
