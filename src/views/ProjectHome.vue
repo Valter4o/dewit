72ebc463c4d2594ba69c7917ef9639feab1c7db0
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-12 10:55:14 +0200

    Added skeleton loader

--- src/views/ProjectHome.vue
+++ src/views/ProjectHome.vue
@@ -1,34 +1,78 @@
 <template>
-  <div v-if="asyncDataStatus_ready">
-    <h1>{{ project.name }}</h1>
-    <template>
-      <v-card class="mx-auto" max-width="700">
-        <v-card-text>
-          <h1>Project Description</h1>
-          <br />
-          <p>
-            {{ project.description }}
-          </p>
-        </v-card-text>
-        <v-card-actions :class="$style.buttonsContainer">
-          <router-link :to="{ name: 'UsersList' }">
-            <v-btn text rounded="rounded" max-width="200px" color="deep-purple">
-              Users List
-            </v-btn>
-          </router-link>
-          <router-link :to="{ name: 'TodoGroups', params: { id } }">
-            <v-btn text rounded="rounded" max-width="200px" color="deep-purple">
-              Todo List
-            </v-btn>
-          </router-link>
-          <router-link :to="{ name: 'Tasker' }">
-            <v-btn text rounded="rounded" max-width="200px" color="deep-purple">
-              Tasker
-            </v-btn>
-          </router-link>
-        </v-card-actions>
-      </v-card>
-    </template>
+  <div>
+    <div v-if="asyncDataStatus_ready">
+      <h1>{{ project.name }}</h1>
+      <template>
+        <v-card class="mx-auto" max-width="700">
+          <v-card-text>
+            <h1>Project Description</h1>
+            <br />
+            <p>
+              {{ project.description }}
+            </p>
+          </v-card-text>
+          <v-card-actions :class="$style.buttonsContainer">
+            <router-link :to="{ name: 'UsersList' }">
+              <v-btn
+                text
+                rounded="rounded"
+                max-width="200px"
+                color="deep-purple"
+              >
+                Users List
+              </v-btn>
+            </router-link>
+            <router-link :to="{ name: 'TodoGroups', params: { id } }">
+              <v-btn
+                text
+                rounded="rounded"
+                max-width="200px"
+                color="deep-purple"
+              >
+                Todo List
+              </v-btn>
+            </router-link>
+            <router-link :to="{ name: 'Tasker' }">
+              <v-btn
+                text
+                rounded="rounded"
+                max-width="200px"
+                color="deep-purple"
+              >
+                Tasker
+              </v-btn>
+            </router-link>
+          </v-card-actions>
+        </v-card>
+      </template>
+    </div>
+    <div v-else="">
+      <v-skeleton-loader
+        :loading="loading"
+        :transition="transition"
+        type="card-heading"
+        width="700"
+        class="mx-auto"
+      >
+      </v-skeleton-loader>
+      <v-skeleton-loader
+        :loading="loading"
+        :transition="transition"
+        type="article"
+        width="700"
+        class="mx-auto"
+      >
+      </v-skeleton-loader>
+      <v-skeleton-loader
+        :loading="loading"
+        :transition="transition"
+        height="500"
+        type="actions"
+        width="700"
+        class="mx-auto"
+      >
+      </v-skeleton-loader>
+    </div>
   </div>
 </template>
 
@@ -40,6 +84,22 @@ export default {
   data() {
     return {
       rounded: true,
+      loading: true,
+      transition: 'scale-transition',
+      transitions: [
+        {
+          text: 'None',
+          value: undefined,
+        },
+        {
+          text: 'Fade Transition',
+          value: 'fade-transition',
+        },
+        {
+          text: 'Scale Transition',
+          value: 'scale-transition',
+        },
+      ],
     }
   },
 
