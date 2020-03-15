24f1d0348e47244ba1379a0aad89db05e3e9a80f
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-15 02:30:08 +0200

    Created first spec

---
+++ tests/Home.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils'
+import Home from '@/views/Home'
+
+describe('Home.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
