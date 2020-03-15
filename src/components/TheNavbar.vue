3934d8aa29441b1c357a0215c15751544396dbfe
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-15 02:29:14 +0200

    Registered login and register dialogs

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -11,26 +11,39 @@
 
     <v-spacer />
 
-    <div class="text-center">
-      <v-btn rounded color="primary" dark>
-        Login
-      </v-btn>
+    <div class="text-center" v-if="!loggedIn">
+      <Login />
     </div>
 
-    <div class="text-center">
-      <v-btn rounded color="primary" dark>
-        Register
-      </v-btn>
+    <div class="text-center" v-if="!loggedIn">
+      <Register />
+    </div>
+
+    <div class="text-center" v-if="loggedIn">
+      <v-btn rounded color="primary" dark @click="logout">Logout</v-btn>
     </div>
   </v-app-bar>
 </template>
 
 <script>
+import Login from '@/components/LoginDialog'
+import Register from '@/components/RegisterDialog'
+
 export default {
+  components: {
+    Login,
+    Register,
+  },
+  data() {
+    return {
+      loggedIn: false,
+    }
+  },
   methods: {
     showHideSidebar() {
       this.$emit('changeSideBar')
     },
+    logout() {},
   },
 }
 </script>
