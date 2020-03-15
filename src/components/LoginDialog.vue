39f8679e092bad69536ab0177edf249a494d21af
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-15 02:28:35 +0200

    Login dialog created

---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on }">
+      <v-btn rounded color="primary" dark v-on="on">
+        Login
+        <LoginVariant />
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Login to your account</span>
+      </v-card-title>
+      <v-form>
+        <v-container>
+          <v-col cols="12" md="10">
+            <v-text-field v-model="form.email" label="E-mail" required>
+            </v-text-field>
+          </v-col>
+          <v-col cols="12" md="10">
+            <v-text-field
+              type="password"
+              v-model="form.password"
+              label="Password"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-container>
+
+        <v-card-text>
+          Login with?
+          <v-btn rounded color="green" @click="authWithGoogle">
+            Google
+          </v-btn>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="blue darken-1" text @click="dialog = false"
+            >Close</v-btn
+          >
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="login">Login</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import LoginVariant from 'vue-material-design-icons/LoginVariant'
+
+export default {
+  components: {
+    LoginVariant,
+  },
+  data: () => ({
+    dialog: false,
+    form: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  }),
+
+  methods: {
+    login() {},
+    authWithGoogle() {},
+  },
+}
+</script>
