be59f6eb4f114c2cc3ee3d600222e729daf68ede
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-15 16:38:51 +0200

    Added register and login methods and getter

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -9,12 +9,54 @@ export default {
 
     getters: {
         authUser(state, getters, rootState) {
-            return state.authId ? rootState.users.items[state.authId] : null
+            return state.authId ? rootState.user.items[state.authId] : null
         }
     },
 
     actions: {
+        registerUserWithEmailAndPassword({ dispatch }, { username, email, password }) {
+            return firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then((user) => {
+                    return dispatch('user/createUser', { id: user.user.uid, username, email }, { root: true })
+                })
+                .then(() => dispatch('fetchAuthUser'))
+        },
 
+        fetchAuthUser({ dispatch, commit }) {
+            if (firebase.auth().currentUser) {
+                const userId = firebase.auth().currentUser.uid
+                return new Promise((resolve, reject) => {
+                    firebase.firestore().collection('user').doc(userId).onSnapshot((s) => {
+                        if (s) {
+                            return dispatch('user/fetchUser', { id: userId }, { root: true })
+                                .then((user) => {
+                                    commit('setAuthId', userId)
+                                    resolve(user);
+                                })
+                        } else {
+                            resolve(null)
+                        }
+                    })
+                })
+            }
+        },
+
+        signInWithEmailAndPassword({ dispatch }, { email, password }) {
+            return firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(() =>
+                    dispatch('fetchAuthUser'))
+
+        },
+
+        signOut({ commit }) {
+            return new Promise((resolve, reject) => {
+                firebase.auth().signOut()
+                    .then(() => {
+                        commit('setAuthId', null)
+                        resolve();
+                    })
+            })
+        },
     },
     mutations: {
         setAuthId(state, id) {
