f7283c3348c4757daa8c30dcb5e78249afe910b3
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-15 16:39:30 +0200

    Created user module with actions to fetch and create user

---
+++ src/store/modules/user.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+
+export default {
+    namespaced: true,
+    state: {
+        items: {}
+    },
+    actions: {
+        createUser({ state, commit }, { id, email, username }) {
+            return new Promise((resolve, reject) => {
+                const registeredAt = Math.floor(Date.now() / 1000)
+                const usernameLower = username.toLowerCase()
+                email = email.toLowerCase()
+                const user = { email, usernameLower, registeredAt, username, projects: [] }
+
+                firebase.firestore().collection('user').doc(id).set(user)
+                    .then(() => {
+                        commit('setItem', { resource: 'user', id, item: user }, { root: true })
+                        resolve(state.items[id]);
+                    })
+            })
+        },
+
+        fetchUser: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'user', id }, { root: true }),
+    },
+}
