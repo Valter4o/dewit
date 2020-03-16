4411191b1e806adba0649240572349580add212b
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-17 01:28:27 +0200

    Added tasker columns

--- src/views/TaskerMain.vue
+++ src/views/TaskerMain.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    <h1>Hi</h1>
+    <v-row>
+      <Column type="Inbox" color="orange" />
+      <Column type="To do" color="red" />
+      <Column type="In Progress" color="blue" />
+      <Column type="Done" color="green" />
+    </v-row>
   </div>
 </template>
 
 <script>
-export default {}
+import Column from '@/components/ColumnTasker'
+
+export default {
+  components: {
+    Column,
+  },
+  data: () => ({}),
+}
 </script>
