cf4a605e3278b7b9be330618009635eaab975f73
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-17 17:15:48 +0200

    Created dialog for creating tags

---
+++ src/components/TagCreateDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-dialog persistent v-model="dialog">
+    <v-card>
+      <v-text-field v-model="newTag" label="Create Tag"> </v-text-field>
+    </v-card>
+    <v-btn rounded @click="close">Close</v-btn>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    createDialogProp: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      newTag: '',
+    }
+  },
+  computed: {
+    dialog() {
+      return this.createDialogProp
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+  },
+}
+</script>
