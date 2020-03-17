5ba340f73aee3d72200fa581714f398e537fea19
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-17 17:16:50 +0200

    Updated long task

--- src/components/LongTaskDialog.vue
+++ src/components/LongTaskDialog.vue
@@ -1,39 +1,99 @@
 <template>
-  <v-dialog v-model="dialog" overlay-opacity="0" hide-overlay max-width="700">
-    <v-card class="mx-auto" max-width="700" max-height="700" outlined>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <h3>Pesho dava sledniq task</h3>
-          <br />
+  <v-dialog
+    v-model="dialog"
+    max-width="60%"
+    persistent
+    overlay-opacity="0"
+    hide-overlay
+  >
+    <v-card class="mx-auto" outlined>
+      <v-app-bar>
+        <h3>Pesho dava sledniq task</h3>
+        <v-spacer />
+        <v-btn @click="close" rounded>
+          X
+        </v-btn>
+      </v-app-bar>
+      <br />
+      <v-row>
+        <v-col :class="$style.leftCol">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
-        </v-list-item-content>
-      </v-list-item>
+        </v-col>
 
-      <v-card-actions>
-        <span>5</span>
-        <v-spacer></v-spacer>
-        <v-list-item>
-          <v-btn small rounded>Demo tag</v-btn>
-          <v-btn small rounded>Demo tag</v-btn>
-          <v-btn small rounded>Demo tag</v-btn>
-        </v-list-item>
-      </v-card-actions>
+        <v-col>
+          <div>
+            <v-app-bar dense flat color="white">
+              <h3>Tags</h3>
+              <v-spacer />
+              <v-btn rounded @click="changeTagsShow">+</v-btn>
+              <Tags :tagsDialogProp="tagDialog" @closeTags="changeTagsShow" />
+            </v-app-bar>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+            <v-btn :class="$style.tag" small rounded>Demo tag</v-btn>
+          </div>
+        </v-col>
+      </v-row>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+import Tags from '@/components/TagsShowDialog'
+
 export default {
+  components: {
+    Tags,
+  },
   props: {
-    dialog: {
+    dialogProp: {
       type: Boolean,
       required: true,
     },
   },
+
+  data() {
+    return {
+      tagDialog: false,
+    }
+  },
+  computed: {
+    dialog() {
+      return this.dialogProp
+    },
+  },
+
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+
+    changeTagsShow() {
+      this.tagDialog = !this.tagDialog
+    },
+  },
 }
 </script>
+
+<style module>
+.leftCol {
+  margin-left: 20px;
+}
+
+.tag {
+  margin: 5px;
+}
+</style>
