4fd7690e90b09a70eb47aebacbd8cc00937b74af
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-18 22:39:09 +0200

    Created tasker module with methods to fetch and create

--- src/store/modules/tasker.js
+++ src/store/modules/tasker.js
@@ -1,9 +1,46 @@
+import firebase from 'firebase/app'
+
 export default {
+    namespaced: true,
     state: {
         items: {}
     },
 
-    actions: {
+    getters: {
+        filteredTasks: (state, getters, rootState, rootGetters) => (status) => {
+            return Object.values(rootState.tasker.items).filter((task) => task.status === status)
+        },
+    },
 
+    actions: {
+        fetchTasks: ({ commit, state }, { projectId }) => {
+            return new Promise((resolve, reject) => {
+                firebase.firestore()
+                    .collection('tasker')
+                    .doc(projectId)
+                    .collection('tasks')
+                    .onSnapshot((s) => {
+
+                        s.docs.map(d => {
+                            const id = d.id;
+                            commit('setItem', { item: d.data(), id, resource: 'tasker' }, { root: true });
+                            return d.data()
+                        });
+                        resolve(state.items);
+                    })
+            })
+        },
+        createTask(ctx, { projectId, task }) {
+            return new Promise((resolve, reject) => {
+                firebase.firestore()
+                    .collection('tasker')
+                    .doc(projectId)
+                    .collection('tasks')
+                    .add(task)
+                    .then(() => {
+                        resolve();
+                    })
+            })
+        }
     }
 }
