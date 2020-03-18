4bac98054dc2d23ebf14ffe147e4fa7d588331c9
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-18 22:38:30 +0200

    Connected tasks

--- src/views/TaskerMain.vue
+++ src/views/TaskerMain.vue
@@ -1,21 +1,53 @@
 <template>
   <div>
-    <v-row>
-      <Column type="Inbox" color="orange" />
-      <Column type="To do" color="red" />
-      <Column type="In Progress" color="blue" />
-      <Column type="Done" color="green" />
+    <v-row v-if="asyncDataStatus_ready">
+      <Column status="Inbox" color="orange" />
+      <Column status="To do" color="red" />
+      <Column status="In Progress" color="blue" />
+      <Column status="Done" color="green" />
     </v-row>
+    <div v-else>
+      Loading
+    </div>
   </div>
 </template>
 
 <script>
 import Column from '@/components/ColumnTasker'
+import { mapActions } from 'vuex'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
 
 export default {
   components: {
     Column,
   },
-  data: () => ({}),
+  props: {
+    id: {
+      type: String,
+      requiredL: true,
+    },
+  },
+
+  mixins: [asyncDataStatus],
+
+  methods: {
+    fetchTasksMethod() {
+      this.fetchTasks({ projectId: this.id }).then((data) => {
+        this.asyncDataStatus_fetched()
+      })
+    },
+    ...mapActions('tasker', ['fetchTasks']),
+    ...mapActions('projects', ['fetchProject']),
+  },
+
+  created() {
+    if (!this.$store.state.projects.items[this.id]) {
+      this.fetchProject({ id: this.id }).then(() => {
+        this.fetchTasksMethod()
+      })
+    } else {
+      this.fetchTasksMethod()
+    }
+  },
 }
 </script>
