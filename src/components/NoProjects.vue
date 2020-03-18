5cca76773c82e9ace377387d43cad79e2a6af5dc
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-18 22:40:12 +0200

    Added no project component

---
+++ src/components/NoProjects.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card class="mx-auto" max-width="360" min-width="360" flat elevation="5">
+    <v-list-item three-line>
+      <v-list-item-content>
+        <h3>Not tasks in here .... yet :D</h3>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style module></style>
