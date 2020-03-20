45bf5ee02f1f9f1566db19605ebb87ed3c031ce8
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-20 09:14:59 +0200

    This is not the commit u are looking for

--- src/components/NoProjects.vue
+++ src/components/NoProjects.vue
@@ -2,7 +2,7 @@
   <v-card class="mx-auto" max-width="360" min-width="360" flat elevation="5">
     <v-list-item three-line>
       <v-list-item-content>
-        <h3>Not tasks in here .... yet :D</h3>
+        <h3>Theese aren't the tasks you are looking for 👋</h3>
       </v-list-item-content>
     </v-list-item>
   </v-card>
