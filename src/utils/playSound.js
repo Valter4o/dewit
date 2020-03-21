0f42a5f90c728ac6ce9469c4646624bbcf7b8d39
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-21 16:05:48 +0200

    Extracted playSound function in utils folder

---
+++ src/utils/playSound.js
@@ -0,0 +1,8 @@
+export default function (src) {
+    var sound = new Howl({
+        autoplay: true,
+        src: [require(`../assets/sounds/${src}.mp3`)],
+        volume: 0.5,
+    })
+    sound.play()
+}
