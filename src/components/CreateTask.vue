221ca842ca20eebeae03443158346f49ad21e6be
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-03-26 22:40:47 +0200

    Update CreateTask.vue

--- src/components/CreateTask.vue
+++ src/components/CreateTask.vue
@@ -50,25 +50,27 @@ export default {
     submit() {
       const projectId = this.$router.currentRoute.params.id
 
-      this.fetchTodoGroups({ projectId })
-      // this.createTodoGroup().then((id) => {
-      //   const task = {
-      //     title: this.title,
-      //     description: this.description,
-      //     status: 'Inbox',
-      //     todoGroup: id,
-      //     createdAt: Date.now(),
-      //     comments: [],
-      //     tags: [],
-      //   }
-      //   const projectId = this.$router.currentRoute.params.id
+      this.fetchTodoGroups({ projectId }).then(() => {
+        const todoGroups = Object.keys(this.$store.state.todoGroups.items)
+        this.createTodoGroup().then((id) => {
+          const task = {
+            title: this.title,
+            description: this.description,
+            status: 'Inbox',
+            todoGroup: id,
+            createdAt: Date.now(),
+            comments: [],
+            tags: [],
+          }
+          todoGroups.push(id)
 
-      //   this.createTask({ projectId, task,todoGroups }).then(() => {
-      //     this.title = ''
-      //     this.description = ''
-      //     this.close()
-      //   })
-      // })
+          this.createTask({ projectId, task, todoGroups }).then(() => {
+            this.title = ''
+            this.description = ''
+            this.close()
+          })
+        })
+      })
     },
     ...mapActions('tasker', ['createTask', 'fetchTodoGroups']),
     ...mapActions('todoGroups', ['createTodoGroup']),
