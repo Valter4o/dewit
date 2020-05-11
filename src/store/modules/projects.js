5d9dabd31ac003e9957d778e16a164e32cad4659
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-05-11 07:38:36 +0300

    Formated file

--- src/store/modules/projects.js
+++ src/store/modules/projects.js
@@ -1,56 +1,70 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/app'
 
 const getDefaultState = () => {
-    return {
-        items: {},
-    }
+  return {
+    items: {},
+  }
 }
 export default {
-    namespaced: true,
-    state: getDefaultState(),
-    actions: {
-        fetchProjects: ({ dispatch }, { ids }) =>
-            dispatch('fetchItems', {
-                ids, resource: 'projects'
-            }, {
-                root: true
-            }),
-        fetchProject: ({ dispatch }, { id }) =>
-            dispatch('fetchItem', {
-                id, resource: 'projects'
-            }, {
-                root: true
-            }),
-        createProject(_, { project }) {
-            return new Promise((resolve, reject) => {
-                firebase.firestore()
-                    .collection('projects')
-                    .add(project)
-                    .then((id) => {
-                        resolve(id)
-                    })
-            })
+  namespaced: true,
+  state: getDefaultState(),
+  actions: {
+    fetchProjects: ({ dispatch }, { ids }) =>
+      dispatch(
+        'fetchItems',
+        {
+          ids,
+          resource: 'projects',
         },
-        updateProject(_, { project, projectId }) {
-            return firebase.firestore()
-                .collection('projects')
-                .doc(projectId)
-                .update(project)
-        },
-        deleteProject({ commit }, { id }) {
-            return firebase.firestore()
-                .collection('projects')
-                .doc(id)
-                .delete().then(() => {
-                    commit('resetState')
-                    return Promise.resolve()
-                })
+        {
+          root: true,
         }
+      ),
+    fetchProject: ({ dispatch }, { id }) =>
+      dispatch(
+        'fetchItem',
+        {
+          id,
+          resource: 'projects',
+        },
+        {
+          root: true,
+        }
+      ),
+    createProject(_, { project }) {
+      return new Promise((resolve, reject) => {
+        firebase
+          .firestore()
+          .collection('projects')
+          .add(project)
+          .then((id) => {
+            resolve(id)
+          })
+      })
     },
-    mutations: {
-        resetState(state) {
-            Object.assign(state, getDefaultState())
-            return Promise.resolve()
-        }
-    }
-} 
+    updateProject(_, { project, projectId }) {
+      return firebase
+        .firestore()
+        .collection('projects')
+        .doc(projectId)
+        .update(project)
+    },
+    deleteProject({ commit }, { id }) {
+      return firebase
+        .firestore()
+        .collection('projects')
+        .doc(id)
+        .delete()
+        .then(() => {
+          commit('resetState')
+          return Promise.resolve()
+        })
+    },
+  },
+  mutations: {
+    resetState(state) {
+      Object.assign(state, getDefaultState())
+      return Promise.resolve()
+    },
+  },
+}
