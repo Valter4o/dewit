22b3d1f6ff1d0ce315c41f9508caac57962c40c6
Valentin Tashkov <46874613+Valter4o@users.noreply.github.com>
2020-05-12 15:11:38 +0300

    Created common folder for common components

---
+++ src/components/common/TheNavbar.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-app-bar app clipped-left color="black" dense>
+    <v-app-bar-nav-icon @click.stop="showHideSidebar()" />
+    <template>
+      <v-btn @click="redirect('Home')" rounded fab small>
+        <Home />
+      </v-btn>
+    </template>
+
+    <v-spacer />
+    <template v-if="load && username">
+      <hollow-dots-spinner
+        :animation-duration="1000"
+        :dot-size="15"
+        :dots-num="3"
+        color="#f2c92b"
+      />
+    </template>
+    <template v-else>
+      <template v-if="!loggedInId">
+        <div class="text-center">
+          <Login />
+        </div>
+
+        <div class="text-center">
+          <Register />
+        </div>
+      </template>
+      <div class="text-center" v-else>
+        <span>
+          Welcome
+          <v-btn color="#f2c92b" rounded>{{ username }}</v-btn>
+        </span>
+        <v-btn rounded color="#f2c92b" dark @click="logout">Logout</v-btn>
+      </div>
+    </template>
+  </v-app-bar>
+</template>
+
+<script>
+import Login from '@/components/auth/LoginDialog'
+import Register from '@/components/auth/RegisterDialog'
+
+import userData from '@/mixins/userData'
+
+import Home from 'vue-material-design-icons/Home'
+
+import { mapActions, mapGetters } from 'vuex'
+
+import { HollowDotsSpinner } from 'epic-spinners'
+
+export default {
+  components: {
+    Login,
+    Register,
+    Home,
+    HollowDotsSpinner,
+  },
+
+  mixins: [userData],
+
+  data() {
+    return {
+      load: true,
+      username: '',
+    }
+  },
+  methods: {
+    redirect(name) {
+      if (this.$router.currentRoute.name !== name) {
+        this.$router.push({
+          name,
+        })
+      }
+    },
+    async getUsername() {
+      const accesor = (user) => user.username
+      this.username = await this.getUserData(accesor)
+    },
+    showHideSidebar() {
+      this.$emit('changeSideBar')
+    },
+    logout() {
+      this.signOut().then(() => {
+        this.redirect('Home')
+      })
+    },
+    ...mapActions('auth', ['signOut']),
+    ...mapGetters('auth', ['loggedInId']),
+  },
+
+  created() {
+    this.getUsername()
+    setTimeout(() => {
+      this.load = false
+    }, 1000)
+  },
+}
+</script>
+
+<style module>
+.homeBtn {
+  margin-top: 10px;
+  background-color: goldenrod;
+}
+</style>
---
+++ src/components/common/TheSidebar.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <v-navigation-drawer app clipped width="300" color="black">
+      <template v-if="isLoggedIn">
+        <template v-if="id">
+          <br />
+
+          <router-link :to="{ name: 'ProjectHome' }">
+            <v-btn rounded color="green">
+              Go to project details
+            </v-btn>
+          </router-link>
+          <div v-if="asyncDataStatus_ready">
+            <br />
+            <h1>Users in {{ projectName }}</h1>
+            <br />
+            <v-card class="mx-auto">
+              <v-simple-table>
+                <thead>
+                  <tr>
+                    <th class="text-left">Name</th>
+                    <th class="text-left">Role</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="user in userList" :key="user._key">
+                    <td class="text-left">{{ user.name }}</td>
+                    <td class="text-left">{{ user.role }}</td>
+                  </tr>
+                </tbody>
+              </v-simple-table>
+            </v-card>
+          </div>
+        </template>
+        <template v-else>
+          <v-col>
+            <JoinProject />
+            <h2>Or</h2>
+            <CreateProject />
+          </v-col>
+        </template>
+      </template>
+      <template v-else>
+        <h2>Log in in order to be able to create or join projects</h2>
+      </template>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
+import CreateProject from '@/components/creational/ProjectCreateDialog'
+import JoinProject from '@/components/transformers/JoinProjectDialog'
+
+export default {
+  mixins: [asyncDataStatus],
+  components: {
+    CreateProject,
+    JoinProject,
+  },
+  methods: {
+    ...mapGetters('auth', ['authUser']),
+    ...mapActions('projects', ['fetchProject']),
+  },
+
+  computed: {
+    isLoggedIn() {
+      return this.authUser()
+    },
+    userList() {
+      return this.$store.state.projects.items[this.id].users
+    },
+    projectName() {
+      return this.$store.state.projects.items[this.id].name
+    },
+    id() {
+      return this.$router.currentRoute.params.id
+    },
+  },
+
+  created() {
+    if (this.id) {
+      const request = this.fetchProject({ id: this.id })
+      setTimeout(() => {
+        request.then(() => {
+          this.asyncDataStatus_fetched()
+        }, 1000)
+      })
+    }
+  },
+}
+</script>
